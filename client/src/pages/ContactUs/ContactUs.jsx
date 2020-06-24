95bcd6e349c340e3b230d02ef15d0e16b6fc3886
Matthew <matthew_carpenter@icloud.com>
2020-06-24 07:17:31 -0700

    Added Contact Us page.

---
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+// ---------- //
+//   EXPORT   //
+// ---------- //
+export default function Cost() {
+  return (
+    <StyledRoot>
+      <h1>Contact Us</h1>
+    </StyledRoot>
+  );
+}
+
+// ---------- //
+//   STYLES   //
+// ---------- //
+const StyledRoot = styled.main`
+  margin: 0;
+  padding: 1em;
+`;
