8448bf88d7db86d477c13ec19b2390587861cf80
Matthew <matthew_carpenter@icloud.com>
2020-06-28 13:36:26 -0700

    Added components to Price page.

--- client/src/pages/Cost/Cost.jsx
+++ client/src/pages/Cost/Cost.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import Price from "../../components/Price/Price";
 
+// ---------- //
+//   RETURN   //
+// ---------- //
 export default function Cost() {
   return (
     <StyledRoot>
-      <h1>Price Guide</h1>
+      <Price />
     </StyledRoot>
   );
 }
 
+// ---------- //
+//   STYLES   //
+// ---------- //
 const StyledRoot = styled.main`
   margin: 0;
   padding: 1em;
